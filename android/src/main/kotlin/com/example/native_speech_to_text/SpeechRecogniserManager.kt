e7d4041ab10832a890d32e79932efe961dab430f
Madhup Singh Yadav <byteslick@gmail.com>
2025-06-16 15:24:50 +0530

    Dictation Mode enabled for punctuations

--- android/src/main/kotlin/com/example/native_speech_to_text/SpeechRecogniserManager.kt
+++ android/src/main/kotlin/com/example/native_speech_to_text/SpeechRecogniserManager.kt
@@ -27,6 +27,8 @@ class SpeechRecognizerManager(private val activity: Activity) {
         intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
             putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+            putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true)
+            putExtra("android.speech.extra.DICTATION_MODE", true)
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US")
         }
 
